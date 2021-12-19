359e9121c3eb456e4e0bad6dfd232f99079dfbe9
Dave Burke <dburke84@gmail.com>
2021-12-19 09:05:47 -0600

    Remove team solutions

--- solutions/3.ts
+++
@@ -1,6 +0,0 @@
-import { readToArray } from "../utilities/utils";
-
-// Prints answer to challenge to the console
-export const answer = async (file: string) => {
-  console.log("Solution Coming Soon!");
-};
