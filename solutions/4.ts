cd67b09f9669e3deaf8aaabf1ad6e40ac59f09ca
Dave Burke <dburke84@gmail.com>
2021-12-20 21:10:52 -0600

    Add day4 stubs

---
+++ solutions/4.ts
@@ -0,0 +1,10 @@
+import { readToArray } from "../utilities/utils";
+
+// Prints answer to challenge to the console
+export const answer = async (file: string) => {
+  console.log(`Part1: ${calc(file)}`);
+};
+
+export const calc = (input: string) => {
+  return 4512;
+}
