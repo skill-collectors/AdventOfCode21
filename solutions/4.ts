72fba68924b1ebb78f9e859ece49b503a71c5f95
Dave Burke <dburke84@gmail.com>
2021-12-20 21:59:07 -0600

    WIP bingo input parsing

--- solutions/4.ts
+++ solutions/4.ts
@@ -5,6 +5,33 @@ export const answer = async (file: string) => {
   console.log(`Part1: ${calc(file)}`);
 };
 
-export const calc = (input: string) => {
+class Board {
+  rows;
+
+  constructor(input: string) {
+    const lines = input.split('\n')
+    this.rows = lines
+      .map(line => line
+        .trim()
+        .replaceAll(/ +/g, ' ')
+        .split(' ')
+        .map(Number))
+  }
+
+  print() {
+    const lines = this.rows
+      .map(row => row
+        .map(n => n.toString().padStart(2, ' '))
+        .join(' '));
+    const output = lines.join('\n');
+    console.log(output);
+  }
+}
+
+export const calc = (file: string) => {
+  const paragraphs = file.split('\n\n');
+  const numbers = paragraphs.shift()?.split(',').map(Number);
+  const boards = paragraphs.map(board => new Board(board))
+  boards[0].print();
   return 4512;
 }
